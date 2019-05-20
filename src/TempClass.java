0c3a51e2526b9a3c133ec91cbba8299f888b4172
X9Squared <justin.wang91202@gmail.com>
2019-05-20 08:57:05 -0700

    User interface v1, Survivor init

--- src/TempClass.java
+++
@@ -1,7 +0,0 @@
-import static java.lang.System.out;
-
-public class TempClass {
-    public static void main(String[] args) {
-        out.println("Hello World");
-    }
-}
---
+++ src/User.java
@@ -0,0 +1,6 @@
+public interface User {
+    public abstract int[] getLocation(int zip);
+    public abstract int getAge();
+    public abstract int getName();
+    public abstract int setId();
+}
