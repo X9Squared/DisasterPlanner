14f987d146d4db103b80adb8e59325baf3209db6
X9Squared <justin.wang91202@gmail.com>
2019-05-22 08:14:58 -0700

    Provider getters/setters, etc

--- src/UserClasses/Provider.java
+++ src/UserClasses/Provider.java
@@ -1,4 +1,53 @@
 package UserClasses;
 
-public class Provider {
+import ZipCore.ZipCode;
+import ZipCore.ZipList;
+
+public class Provider implements User {
+    private String name;
+    private int id;
+    private ZipCode location;
+    private boolean outofstock;
+    private String service;
+    private ZipList zips;
+
+    //Constructors
+    public Provider() {
+        name = "Wal-Mart";
+        id = 0000;
+        location = new ZipCode(92130);
+        outofstock = false;
+        service = "Literally ANYTHING";
+        zips = new ZipList();
+    }
+
+    public Provider(int z, String n, int i, ZipCode loc, int age, boolean s, String serv, String med) {
+        name = n;
+        id = i;
+        location = loc;
+        outofstock = s;
+        service = req;
+        zips = new ZipList();
+    }
+
+    //Set and get methods (integrated ZipCore at a later date)
+    public ZipCode getLocation(int zip) {
+        return location;
+    }
+
+    @Override
+    public ZipCode setLoc(int zip) {
+        return null;
+    }
+
+    @Override
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public void setId(int newId) {
+        this.id = newId;
+    }
+}
 }
--- src/UserClasses/User.java
+++ src/UserClasses/User.java
@@ -5,8 +5,6 @@ import ZipCore.ZipCode;
 public interface User {
     public abstract ZipCode setLoc(int zip);
 
-    public abstract int getAge();
-
     public abstract String getName();
 
     public abstract void setId(int newId);
