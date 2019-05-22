601283e262cc8409a5f14138a9da5b833ed50aa2
Justin Wang <justin.wang91202@gmail.com>
2019-05-22 16:54:33 -0700

    searchClosestSurvivor method complete

--- src/UserClasses/SurvivorList.java
+++ src/UserClasses/SurvivorList.java
@@ -10,22 +10,26 @@ public class SurvivorList {
     private ArrayList<Survivor> survivors;
     private ZipList zips;
 
-    public ArrayList<String> searchClosestSurvivors(String service, ZipCode zip, double threshhold) {
+    public ArrayList<String> searchClosestSurvivors(String service, ZipCode zip, double threshold) {
         ArrayList<String> output = new ArrayList<>();
+        ArrayList<String> contingency = new ArrayList<>();
+        contingency.add("No survivors found within the specified threshold - please try again");
         boolean found = false;
-        Provider closestProv = new Provider();
         for (Survivor surv : survivors) {
-            if (surv.().equals(service)) {
+            if (surv.getNeed().equals(service)) {
                 found = true;
                 ZipCode temp = zips.searchLoc(zip.getZipcode());
                 double tempDist = temp.calcDistance(zip);
-                if (tempDist < minDistance) {
-                    minDistance = tempDist;
-                    closestProv = prov;
+                if (tempDist < threshold) {
+                    output.add(surv.toString());
                 }
             }
         }
-        return output;
+        if (!found) {
+            return contingency;
+        } else {
+            return output;
+        }
     }
 
     public ArrayList<Survivor> getSurvivors() {
